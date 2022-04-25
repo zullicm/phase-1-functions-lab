fc7faded40a403f56477f3933eb70381f3b500fa
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-25 15:15:42 -0400

    commit tree b23788757e2c63fdd3c4cc39e833366df04c46fc

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+//total 13 need to pass
+
+//3 of 3 passing 
+function distanceFromHqInBlocks(street){
+    if(street < 42){
+        return 42 - street
+    }else if(street > 42){
+        return street - 42
+    }
+}
+
+//3 of 3 passing 
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street) * 264
+}
+
+//3 of 3 passing
+function distanceTravelledInFeet(start, end){
+    if (start > end){
+        return (start - end) * 264
+    }else if(start < end){
+        return (end - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, end){
+    if (distanceTravelledInFeet(start,end) < 400){
+        return 0
+    }else if(distanceTravelledInFeet(start,end) < 2000){
+        return (distanceTravelledInFeet(start, end) - 400) * 0.02
+    }else if(distanceTravelledInFeet(start,end) <= 2500){
+        return 25
+    }else if(distanceTravelledInFeet(start,end) > 2500){
+        return 'cannot travel that far'
+    }
+}
